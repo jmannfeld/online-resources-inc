6db32d42bdc096b3c3b0bedfe3563789076e99cf
Jordan Mannfeld <jordanmannfeld@gmail.com>
2021-11-28 23:32:46 -0500

    contactForm troubleshooting

--- web/components/sections/ContactForm.js
+++ web/components/sections/ContactForm.js
@@ -3,42 +3,51 @@ import styles from './ContactForm.module.css';
 
 function ContactForm() {
   return (
-    <form
-      name="Contact Form"
-      method="POST"
-      data-netlify="true"
-      netlify-honeypot="bot-field"
-      action="/"
-      className={styles.contactFormContainer}
-    >
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{' '}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <div>
+      <a href="/">Link to Home</a>
+      <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message"></textarea>
+      </form>
+      <form
+        name="Contact Form"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        action="/"
+        className={styles.contactFormContainer}
+      >
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:{' '}
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
   );
 }
 export default ContactForm;
