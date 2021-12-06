84a1129a3b22f07d2ce25c28544744a37f102565
Jordan Mannfeld <jordanmannfeld@gmail.com>
2021-12-06 16:50:10 -0500

    uses Fjalla One font

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -30,6 +30,10 @@ function Layout(props) {
           href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         ></link> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap"
+          rel="stylesheet"
+        ></link>
         <link rel="stylesheet" href="https://use.typekit.net/cqf5prn.css"></link>
         <link rel="shortcut icon" href="../static/favicon.ico" />
       </Head>
